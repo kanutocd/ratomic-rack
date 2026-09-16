e762678019c861e74a56708cc355617b495bd522
Ken C. Demanawa <kenneth.c.demanawa@gmail.com>
2026-09-16 16:12:51 +0800

    chore: add initial boilerplate files from bundle gem command

---
+++ lib/ratomic/rack.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "rack/version"
+
+module Ratomic
+  module Rack
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/ratomic/rack/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Ratomic
+  module Rack
+    VERSION = "0.1.0"
+  end
+end
